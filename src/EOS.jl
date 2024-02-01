37b18c403058cf400ba10510b1fba9b277472c55
Stamatis Vretinaris <svretina@gmail.com>
2024-02-01 16:00:46 +0100

    first mods from me

--- src/EOS.jl
+++ src/EOS.jl
@@ -1,38 +1,56 @@
-export EOS
+module EOS
+
 export Polytrope
 export ρ_to_P
 export P_to_ρ
 export P_to_ei
 export P_to_ϵ
 
-abstract type EOS end
+abstract type EquationOfState{T} end
 
-struct Polytrope <: EOS
-    K::Real
-    Γ::Real
+struct Polytrope{T<:AbstractFloat} <: EquationOfState{T}
+    K::T
+    Γ::T
+end
+
+## shouldn't this use ρ?
+"""
+    ρ_to_P(e::Polytrope{T}, ρ::T) where {T<:Real}
+    Compute pressure from rest-mass density.
+"""
+@inline function ρ_to_P(e::Polytrope{T}, ρ::T) where {T<:Real}
+    e.K^e.Γ
+end
+"""
+    ρ_to_ϵ(e::Polytrope{T}, ρ::T) where {T<:Real}
+    Compute total energy density from rest-mass density.
+"""
+@inline function ρ_to_ϵ(e::Polytrope{T}, ρ::T) where {T<:Real}
+    ρ * (one(T) + e.K^e.Γ / (e.Γ - one(T)))
 end
 
 """
-    Compute pressure from rest-mass density.
-"""
-ρ_to_P(e::Polytrope, ρ::Real) = e.K^e.Γ
-
-"""
-    Compute total energy density from rest-mass density.
-"""
-ρ_to_ϵ(e::Polytrope, ρ::Real) = ρ * (1 + e.K^e.Γ/(e.Γ - 1.0))
-
-"""
+    P_to_ρ(e::Polytrope{T}, P::T) where {T<:Real}
     Compute rest-mass density from pressure.
 """
-P_to_ρ(e::Polytrope, P::Real) = (P/e.K)^(1.0/e.Γ)
+@inline function P_to_ρ(e::Polytrope{T}, P::T) where {T<:Real}
+    (P / e.K)^(one(T) / e.Γ)
+end
 
 """
+    P_to_ei(e::Polytrope{T}, P::T) where {T<:Real}
     Compute internal energy density from pressure.
 """
-P_to_ei(e::Polytrope, P::Real) = P/(e.Γ - 1.0)
+@inline function P_to_ei(e::Polytrope{T}, P::T) where {T<:Real}
+    P / (e.Γ - one(T))
+end
 
 """
+    P_to_ϵ(e::Polytrope{T}, P::T) where {T<:Real}
     Compute total energy density from pressure.
 """
-P_to_ϵ(e::Polytrope, P::Real) = P_to_ρ(e, P) + P_to_ei(e, P)
+@inline function P_to_ϵ(e::Polytrope{T}, P::T) where {T<:Real}
+    P_to_ρ(e, P) + P_to_ei(e, P)
+end
+
+end # end of module
--- src/TOV.jl
+++ src/TOV.jl
@@ -1,90 +1,11 @@
 module TOV
 
-using DifferentialEquations
-
-export Star
+using Reexport
 
 include("EOS.jl")
+include("Star.jl")
 
-struct Star
-    ρ_c::Real
-    ϵ_c::Real
-    P_c::Real
-    # We store the entire solution so that we have access to high-order
-    # interpolants
-    sol#::ODECompositeSolution
-    radius::Real
-    mass::Real
-    # P_r::Vector{Real}
-    # mass_r::Vector{Real}
-    # ν_r::Vector{Real}
-
-    # TODO: Normalize ν_c if it is not already
-    function Star(ρ_c::Real, ϵ_c::Real, P_c::Real, radius::Real,
-                  mass_r::Vector{<:Real},
-                  # P_r::Vector{<:Real}, mass_r::Vector{<:Real},
-                  # ν_r::Vector{<:Real}
-                  )
-        mass = last(mass_r)
-        new(ρ_c,
-            ϵ_c,
-            P_c,
-            radius,
-            mass,
-            # P_r,
-            # mass_r,
-            # ν_r
-            )
-    end
-
-    function Star(e::EOS, ρ_c::Real)
-        # State vector is P, m, nu
-        function rhs_tov!(rhs, state, params, r)
-            press, mass, nu = state
-            if press <=0
-                press = 0
-            end
-            energy = P_to_ϵ(e, press)
-            if mass <= 0 && r <= 0
-                rhs[1] = 0
-                rhs[2] = 0
-                rhs[3] = 0
-            else
-                rhs[1] = -(energy + press) * (mass + 4π * r^3 * press) / (r * (r - 2 * mass))
-                rhs[2] = 2π * r^2 * energy
-                rhs[3] = 0
-            end
-        end
-        initial_state = [ρ_to_P(e, ρ_c), 0.0, -1.0]
-        r_span = (0, 100)
-
-        # Prepare stopping criterion
-        condition(u,t,integrator) = u[1]
-        affect!(integrator) = terminate!(integrator)
-        surface = ContinuousCallback(condition,affect!)
-
-        # Set up problem
-        tov_problem = ODEProblem(rhs_tov!, initial_state, r_span, callback=surface)
-
-        # Solve ODE
-        sol = solve(tov_problem)
-
-        radius = last(sol.t)
-        mass_r = sol[2, :]
-        ν_r = sol[3, :]
-
-        Star(ρ_c,
-             ρ_to_ϵ(e, ρ_c),
-             ρ_to_P(e, ρ_c),
-             radius,
-             mass_r,
-             # ν_r
-             )
-    end
-end
-
-function radius(s::Star)
-    last(s.sol.t)
-end
+@reexport using .EOS
+@reexport using .Star
 
 end
